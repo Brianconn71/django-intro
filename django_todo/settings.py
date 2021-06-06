aeecda062519b76ab1782d5e8e14bf2a67a251b9
Brianconn71 <Brian.connolly71@gmail.com>
2021-06-06 11:22:40 +0000

    added setup confidurations and db configurations with heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://ityufvwbapwdcj:e8e4a521442f5a1f0a994cc5f9622a2183e0fab0e63cb2a5d973b5b6dd057c04@ec2-3-218-71-191.compute-1.amazonaws.com:5432/d6sbca69omlvvj')
 }
 
 
